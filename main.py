177420266e411efd8882082aa9d45d492ce3c02e
Meysam Alipuor <meysam.alipuor@gmail.com>
2019-07-08 12:36:17 +0430

    improvement

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from DBSCAN import DBSCAN
 from Cluster import Cluster
 from Point import Point
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = np.array([
+dataset1 = np.array([
     # classter 1
     [26, 25],
     [34, 23],
@@ -18,21 +17,21 @@ dataset = np.array([
     [34, 18],
 
     # claster2
-    [66, 10],
-    [67, 30],
-    [66, 31],
-    [67, 23],
-    [70, 28],
     [65, 11],
-    [72, 18],
+    [66, 10],
+    [66, 31],
+    [66, 33],
+    [66, 17],
+    [67, 30],
+    [67, 23],
     [69, 19],
     [69, 15],
-    [74, 26],
+    [70, 28],
     [71, 29],
+    [72, 18],
+    [74, 26],
     [68, 21],
-    [66, 31],
     [73, 9],
-    [67, 17],
 
     # claster3
     [50, 60],
@@ -65,18 +64,20 @@ dataset = np.array([
     [25, 70],
     [25, 50],
     [50, 10],
-    [44, 42],
+    [44, 45],
     [42, 42],
 
 ])
 
-noise = np.array([
-    [25, 70],
-    [25, 50],
-    [50, 10],
-    [44, 42],
-    [42, 42],
-])
+dataset_list = list()
+
+read_file = open('dataset1.txt').readlines()
+for row in read_file:
+    record = []
+    for item in row.split(',')[0].split(';'):
+        record.append(int(item))
+    dataset_list.append(record)
+dataset = np.array(dataset_list)
 
 epsilon = 10
 minPoints = 3
@@ -84,51 +85,68 @@ minPoints = 3
 clusters = []
 
 points = []
+unvisited_points = []
+
 for data in dataset:
-    points.append(Point(data[0], data[1]))
+    p = Point(data[0], data[1])
+    points.append(p)
+    unvisited_points.append(p)
 
-# start_point_index = np.random.randint(0, len(points))
-start_point_index = 52
-start_point = points[start_point_index]
-
-print(start_point, start_point_index)
-print(points[42])
-print(points[42])
-
-neighbours = []
-
-# for point in points:
-#     point.setVisited(True)
-#     if abs(point.x() - start_point.x()) <= epsilon and abs(point.y() - start_point.y()) <= epsilon:
-#         neighbours.append(point)
-
-for point in points:
-    if abs(point.x() - start_point.x()) <= epsilon and abs(point.y() - start_point.y()) <= epsilon:
-        neighbours.append(point)
-
-
-if len(neighbours) < minPoints:
-    for point in neighbours:
-        point.setNoise(True)
-        point.setVisited(True)
+while len(unvisited_points) > 0:
     neighbours = []
-else:
-    cluster = Cluster()
-    cluster.addPoints(neighbours)
-    clusters.append(cluster)
 
-    while len(neighbours) > 1:
-        selected_neighbour = neighbours.pop()
-        cluster.addPoint(selected_neighbour)
-        selected_neighbour.setVisited(True)
+    start_point_index = np.random.randint(0, len(unvisited_points))
+    start_point = unvisited_points[start_point_index]
 
-        for point_1 in points:
-            if point_1.getVisited() == False and abs(point_1.x() - selected_neighbour.x()) <= epsilon and abs(
-                    point_1.y() - selected_neighbour.y()) <= epsilon:
-                neighbours.append(point_1)
-                # cluster.addPoint(point_1)
+    start_point.setVisited(True)
+    if start_point in unvisited_points:
+        unvisited_points.remove(start_point)
 
-print(len(clusters))
-# plt.scatter(dataset[:, 0], dataset[:, 1])
-# plt.scatter(noise[:, 0], noise[:, 1])
-# plt.show()
+    for point in unvisited_points:
+        if abs(point.x() - start_point.x()) <= epsilon and abs(point.y() - start_point.y()) <= epsilon:
+            neighbours.append(point)
+
+    if len(neighbours) < minPoints - 1:
+        print('noise found')
+        for point in neighbours:
+            point.setNoise(True)
+            point.setVisited(True)
+            if point in unvisited_points:
+                unvisited_points.remove(point)
+        neighbours = []
+    else:
+        cluster = Cluster()
+        cluster.addPoints(neighbours)
+        cluster.addPoint(start_point)
+        clusters.append(cluster)
+
+        while len(neighbours) > 0:
+            selected_neighbour = neighbours.pop()
+            cluster.addPoint(selected_neighbour)
+            selected_neighbour.setVisited(True)
+            if selected_neighbour in unvisited_points:
+                unvisited_points.remove(selected_neighbour)
+
+            for point_1 in unvisited_points:
+                if point_1.getVisited() == False and abs(point_1.x() - selected_neighbour.x()) <= epsilon and abs(
+                        point_1.y() - selected_neighbour.y()) <= epsilon:
+                    neighbours.append(point_1)
+
+print("Cluster count: {}" . format(len(clusters)))
+
+plt.scatter(dataset[:, 0], dataset[:, 1])
+cluster_count = 0
+point_count = 0
+
+for cluster in clusters:
+    cluster_count += 1
+    cluster_points = np.array([[p.x(), p.y()] for p in cluster.points()])
+    plt.scatter(cluster_points[:, 0], cluster_points[:, 1])
+    for p in cluster.points():
+        point_count += 1
+
+    print("Cluster {}th: {}" . format(cluster_count, len(cluster.points())))
+
+print("Noise count: {}" . format(len(dataset) - point_count))
+
+plt.show()
