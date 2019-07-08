593d3020ea14792dd2055c4aa08b5d971632604b
Meysam Alipuor <meysam.alipuor@gmail.com>
2019-07-08 17:05:01 +0430

    improvement

--- DBSCAN.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-class DBSCAN:
-    def __init__(self, dataset):
-        pass
--- main.py
+++ main.py
@@ -5,22 +5,34 @@ import matplotlib.pyplot as plt
 
 dataset_list = list()
 
+"""
+Read dataset from text file
+"""
 read_file = open('dataset1.txt').readlines()
 for row in read_file:
     record = []
     for item in row.split(',')[0].split(';'):
         record.append(int(item) * 10)
     dataset_list.append(record)
+
+"""
+Convert dataset to array
+"""
 dataset = np.array(dataset_list)
 
 epsilon = 75
 minPoints = 3
 
+"""List of clusters"""
 clusters = []
 
+"""List of points"""
 points = []
 unvisited_points = []
 
+"""
+Convert dataset to objects
+"""
 for data in dataset:
     p = Point(data[0], data[1])
     points.append(p)
@@ -29,17 +41,21 @@ for data in dataset:
 while len(unvisited_points) > 0:
     neighbours = []
 
+    """Select Random start point"""
     start_point_index = np.random.randint(0, len(unvisited_points))
     start_point = unvisited_points[start_point_index]
+
 
     start_point.setVisited(True)
     if start_point in unvisited_points:
         unvisited_points.remove(start_point)
 
+    """Find start point neighbourhoods"""
     for point in unvisited_points:
         if abs(point.x() - start_point.x()) <= epsilon and abs(point.y() - start_point.y()) <= epsilon:
             neighbours.append(point)
 
+    """Check if neighbours less than min points"""
     if len(neighbours) < minPoints - 1:
         start_point.setNoise(True)
         # for point in neighbours:
@@ -49,18 +65,30 @@ while len(unvisited_points) > 0:
         #         unvisited_points.remove(point)
         neighbours = []
     else:
+        """Else if neighbours equal or more than min points"""
+
+        """Make a new cluster"""
         cluster = Cluster()
+
+        """Add neighbours to created cluster"""
         cluster.addPoints(neighbours)
         cluster.addPoint(start_point)
         clusters.append(cluster)
 
+        """Check all neighbours from selected start point"""
         while len(neighbours) > 0:
+
+            """Pop a neighbour"""
             selected_neighbour = neighbours.pop()
+
+            """Add selected neighbour to cluster"""
             cluster.addPoint(selected_neighbour)
+
             selected_neighbour.setVisited(True)
             if selected_neighbour in unvisited_points:
                 unvisited_points.remove(selected_neighbour)
 
+            """Find all neighbours of was pop neighbour"""
             for point_1 in unvisited_points:
                 if point_1.getVisited() == False and abs(point_1.x() - selected_neighbour.x()) <= epsilon and abs(
                         point_1.y() - selected_neighbour.y()) <= epsilon:
@@ -72,6 +100,7 @@ plt.scatter(dataset[:, 0], dataset[:, 1])
 cluster_count = 0
 point_count = 0
 
+"""Plot all data on a page"""
 for cluster in clusters:
     cluster_count += 1
     cluster_points = np.array([[p.x(), p.y()] for p in cluster.points()])
